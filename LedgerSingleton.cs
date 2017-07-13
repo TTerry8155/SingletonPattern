37eaeaf25fed00c6da64afbc481ffa9ecafac3f7
TTerry8155 <tterry8155@gmail.com>
2017-07-13 14:53:59 -0500

    Added LedgerSingleton

---
+++ LedgerSingleton.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SingletonPattern.cs
+{
+    public sealed class LedgerSingleton
+    {
+        private static LedgerSingleton _instance = null;
+        //Thread-Safefty
+        private static readonly object padlock = new object();
+
+        LedgerSingleton()
+        {
+
+        }
+
+        public static LedgerSingleton Instance
+        {
+            get
+            {
+                lock (padlock)
+                {
+                    if (Instance == null)
+                    {
+                        _instance = new LedgerSingleton();
+                    }
+                    return _instance;
+                }
+            }
+        }
+    }
+}
